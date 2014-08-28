ed643fa790001adaf625d6ac0a08955f1f87223e
adalee2future <Ada@AdaLeedeMacBook-Pro.local>
2014-08-28 23:02:56 +0800

    add some code

---
+++ floor_devision.py
@@ -0,0 +1,5 @@
+a = 9.0
+b = 4.0
+
+print a / b
+print a // b # floor devision, will get 2.0
---
+++ some_math_functions.py
@@ -0,0 +1,35 @@
+import math
+
+print abs(-2.3)
+print abs(2.3)
+print math.fabs(-2.3)
+print math.fabs(2.3)
+
+
+print math.ceil(9.99)
+print math.floor(9.99)
+
+print cmp(3, 4) # -1 if 3 < 4, 0 if equal, 1 if 3 > 4
+
+print math.exp(1) # the exponential of 1
+
+print math.log(10)  
+print math.log10(10)
+
+print max(1, 2, 3, 4, 7, 5, 9, 8, 0) 
+print min(1, 2, 3, 4, 7, 5, 9, 8, 0) 
+
+# The fractional and integer parts of x in a two-item tuple.
+# Both parts have the same sign as x. 
+# The integer part is returned as a float.
+print math.modf(3.14)
+print math.modf(999)
+
+print pow(3, 4) # x ** y 
+
+print round(9.9912, 2)
+print round(9.9912, 1)
+
+print math.sqrt(16)
+
+ 
