097fbd0acb780f582eaa0ac0d1e596156b7d4d00
adalee2future <Ada@AdaLeedeMacBook-Pro.local>
2014-08-30 09:04:27 +0800

    add some code

---
+++ trigonometric_functions.py
@@ -0,0 +1,51 @@
+import math
+
+# acos
+print math.acos(-1)
+print math.acos(-0.5)
+print math.acos(0)
+print math.acos(0.5)
+print math.acos(1)
+
+# asin
+print math.asin(-1)
+print math.asin(-0.5)
+print math.asin(0)
+print math.asin(0.5)
+print math.asin(1)
+
+# atan
+print math.atan(100)
+print math.atan(-999)
+print math.atan(0)
+
+# atan2, atan2(x, y) = atan(y / x) 
+print math.atan2(4, 5)
+print math.atan2(40, 50)
+
+# cos
+print math.cos(math.pi / 2)
+
+# sin
+print math.sin(math.pi / 2)
+
+# tan
+print math.tan(math.pi / 4)
+
+# degrees
+# converts angle x from radians to degrees
+print math.degrees(math.pi)
+
+# redians
+# converts angle x from degrees to radians.
+print math.radians(180)
+
+# hypot
+# return the Euclidean norm, sqrt(x*x + y*y).
+print math.hypot(3, 4)
+
+# some math constant
+print math.pi
+print math.e
+
+
