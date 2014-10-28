5da6e800ee810ab9ed8ce2c1e6f69a10f50f8c7a
adalee2future <Ada@AdaLeedeMacBook-Pro.local>
2014-10-28 18:44:19 +0800

    add some code

---
+++ codeskulptor/tield_images.py
@@ -0,0 +1,51 @@
+'''
+CARD_SIZE = (73, 98)
+CARD_CENTER = (36.5, 49)
+
+card_pos = (CARD_CENTER[0] + i * CARD_SIZE[0], CARD_CENTER[1] + j * CARD_ZISE[1])
+
+canvas.draw_image(tiled_image, card_pos, CARD_SIZE, ...)
+'''
+
+# demo for drawing using tiled images
+
+import simplegui
+
+# define globals for cards
+RANKS = ('A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K')
+SUITS = ('C', 'S', 'H', 'D')
+
+# cards sprite - 950*392
+CARD_CENTER = (36.5, 49)
+CARD_SIZE = (73, 98)
+card_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/cards.jfitz.png")
+
+# define card class
+class Card:
+    def __init__(self, suit, rank):
+        self.rank = rank
+        self.suit = suit
+        
+    def draw(self, canvas, loc):
+        i = RANKS.index(self.rank)
+        j = SUITS.index(self.suit)
+        card_pos = [CARD_CENTER[0] + i * CARD_SIZE[0],
+                    CARD_CENTER[1] + j * CARD_SIZE[1]]
+        canvas.draw_image(card_image, card_pos, CARD_SIZE, loc, CARD_SIZE)
+        
+# define draw handler
+def draw(canvas):
+    one_card.draw(canvas, (155, 90))
+    
+# define draw handler
+def draw(canvas):
+    one_card.draw(canvas, (155, 90))
+    
+# define frame and register draw handler
+frame = simplegui.create_frame("Card draw", 300, 200)
+frame.set_draw_handler(draw)
+
+# create card
+one_card = Card('D', '6')
+# start frame
+frame.start()
