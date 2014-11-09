2abae5cfdd74329916859e0c78ee7e31bcd03786
adalee2future <Ada@AdaLeedeMacBook-Pro.local>
2014-11-09 10:18:10 +0800

    add some code

---
+++ codeskulptor/collisions_for_sprites.py
@@ -0,0 +1,18 @@
+'''
+group-collide
+    rs = set()
+    for s in myset:
+        rs.add(s)
+        
+    myset.d_u(rs)
+    
+    for s in list(my_set):
+        myset.remove(s)
+        
+missiles - rocks
+set         set
+
+group-group-collide(g1, g2)
+    group-collide(...)
+    return # of collisions
+'''
---
+++ codeskulptor/sprite_animation.py
@@ -0,0 +1,44 @@
+'''
+Animation
+    Flip book - sequence of drawings on pages of book, flip through images, appear to move
+    
+    Relies on phenomenon known as persistence of vision
+    
+    Create the illusion that continuous motion is being seen rather than a series of
+    discontinuous images being exchanged in succession (e.g; 24 frames per sec in movies)
+    
+    Example - horizontally tiled image consisting of 64 images of tumbling asteroid
+    
+    Animate tumbling asteroid - draw each sub-image briefly in sequence 
+'''
+
+# demo of animation using asteroid sprite sheet
+
+import simplegui
+
+# load 64 frame sprite sheer for asteroid - image source is opengameart, artist is warspawn 
+ROCK_CENTER = [64, 64]
+ROCK_SIZE = [128, 128]
+ROCK_DIM = 64
+rock_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/asteroid1.opengameart.warspawn.png")
+
+# global time for animation
+time = 0
+
+# draw handler    
+def draw(canvas):
+    global time
+    current_rock_index = (time % ROCK_DIM) // 1
+    current_rock_center = [ROCK_CENTER[0] +  current_rock_index * ROCK_SIZE[0], ROCK_CENTER[1]]
+    canvas.draw_image(rock_image, current_rock_center, ROCK_SIZE, ROCK_CENTER, ROCK_SIZE) 
+    time += 0.2
+    
+# create frame and size frame based on 128x128 pixel sprite
+frame = simplegui.create_frame("Asteroid sprite", ROCK_SIZE[0], ROCK_SIZE[1])
+
+# set draw handler and canvas background using custom HTML color
+frame.set_draw_handler(draw)
+frame.set_canvas_background("Blue")
+
+# start animation
+frame.start()
