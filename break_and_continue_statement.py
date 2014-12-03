61c788a790e0bbac70eb7517dd7388fd15919c2e
lili545442483 <lili545442483@163.com>
2014-12-03 15:00:11 +0800

    add some code

--- break_and_continue_statement.py
+++ break_and_continue_statement.py
@@ -2,7 +2,7 @@ while True:
     line = raw_input('> ')
     if line == 'done':
         break
-    if line[0] == '#':
+    if line == '' or line[0] == '#':
         continue
     print line
 print 'done'
