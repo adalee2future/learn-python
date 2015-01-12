4ba25964e606e9b8f428b617003fb71e1f47349a
unknown <A5408@IT-A5408.gaptl.com>
2015-01-12 17:15:32 +0800

    add some code

---
+++ temp.py
@@ -0,0 +1,12 @@
+# python 3
+def count_lines(filename):
+	"""
+	Count the number of lines in a file. If the file can't be 
+	opened, it should be treated the same as if it was empty.
+	"""
+	try:
+		return len(open(filename, 'r').readlines())
+	except IOError:
+		## Something went wrong reading the file
+		# or calculating the number of lines
+		return 0
